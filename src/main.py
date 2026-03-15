13667004dc3e02845d48379b2bfe32e893c3b85b
DenisNosik <denisnosik96@gmail.com>
2026-03-15 17:44:50 +0000

    generate public dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def generate_destination(destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination) # delete destination dir
+        print(destination, "deleted")
+    os.mkdir(destination)
+    print(destination, "created")
+
+def copy_all_content_to(source, destination):
+    if not os.path.exists(source):
+        raise Exception("source doesn't exist")
+    for item in os.listdir(source):
+        path = os.path.join(source, item)
+        print("work with:", item, "and path is", path)
+        if os.path.isfile(path):
+            print(path, "is a file. Copy to", destination)
+            shutil.copy(path, destination)
+        else:
+            destination_path = os.path.join(destination, item)
+            print(item, "is dir. destination path is", destination_path)
+            if not os.path.exists(destination_path):
+                print(destination_path, "doesn't exist. Creating.")
+                os.mkdir(destination_path)
+            print("Recursion...")
+            copy_all_content_to(path, destination_path)
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    generate_destination("public")
+    copy_all_content_to("static", "public")
 
 if __name__ == "__main__":
     main()
